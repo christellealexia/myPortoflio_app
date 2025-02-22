175901fb5d7ee583e7d0fddee0623dd2afd516a1
Alexia Christelle <christellealexia53@gmail.com>
2025-02-22 21:15:39 +0200

    Fixed the component of change text

--- src/components/Changingtext.tsx
+++ src/components/Changingtext.tsx
@@ -1,15 +1,28 @@
 import React from 'react'
 import { useState,useEffect } from 'react'
 
+
 const words = ["Designer", "Developer", "Engineer"]
 
-function Changingtext() {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+const ChangeText = () => {
+   const [index,setIndex] = useState(0)
 
-export default Changingtext
+  useEffect(() =>{ 
+  const interval = setInterval(()=> {
+  setIndex((prevIndex) => (prevIndex + 1) % words.length)
+}, 2000)
+  return () => clearInterval(interval);
+}, [])
+
+return (
+  <h1 className="text-4xl font-bold">
+    Software{" "}
+    <span key= {words[index]}
+      className="text-blue-500"></span>
+      {words[index]} 
+  </h1>
+)
+};
+export default ChangeText
+
 
